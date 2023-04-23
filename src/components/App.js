df15f2f7d8c8103ec8b5372c5703c00936abe6d0
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-04-23 16:55:04 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React from "react";
+function App() {
+    return (
+      <div>
+        <h1>Welcome to my React App!</h1>
+        <p>This is my first React application.</p>
+      </div>
+    );
+  }
+  export default App;
+
+
+
+
+
+
