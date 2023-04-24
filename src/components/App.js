ac1274fc17fb696c0adc27666d1195ba5bfbecc8
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-04-24 08:08:09 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Question from "./Question";
+import quiz from "../data/quiz";
 function App() {
-    return (
-      <div>
-        <h1>Welcome to my React App!</h1>
-        <p>This is my first React application.</p>
-      </div>
-    );
+  const [questions, setQuestions] = useState(quiz);
+  const [currentQuestionId, setCurrentQuestion] = useState(1);
+  const [score, setScore] = useState(0);
+  const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  function handleQuestionAnswered(correct) {
+    if (currentQuestionId < questions.length) {
+      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+    } else {
+      setCurrentQuestion(null);
+    }
+    if (correct) {
+      setScore((score) => score + 1);
+    }
   }
-  export default App;
-
-
-
-
-
-
+  return (
+    <main>
+      <section>
+        {currentQuestion ? (
+          <Question
+            question={currentQuestion}
+            onAnswered={handleQuestionAnswered}
+          />
+        ) : (
+          <>
+            <h1>Game Over</h1>
+            <h2>Total Correct: {score}</h2>
+          </>
+        )}
+      </section>
+    </main>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
-
-
-
-
+import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+  // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+    return function cleanUp() {
+      clearTimeout(timerID);
+    };
+  }, [timeRemaining, onAnswered]);
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+  const { id, prompt, answers, correctIndex } = question;
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+export default Question;
